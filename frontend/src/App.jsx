da2201b943843c5b8729c461b56c533b360a938c
Ravindu200232 <it22098450@my.sliit.lk>
2025-01-20 22:59:27 +0530

    create frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+  
+
+  return (
+    <>
+    <ProductCard name="Audio Setup" price="2250/-" description = "leorearfnn sadlkfnsnasd" 
+    img = "https://www.unitedbreweries.com/images/our-brands/heineken.jpg"/>
+    </>
+  )
+}
+
+export default App
