f5599f34d400de4d2aa8006e2e8025fbe55802ef
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2025-04-18 00:44:15 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -26,7 +26,7 @@ app.use((req,res,next)=>{
 
     if(token!=null){
         token = token.replace("Bearer ","");
-        jwt.verify(token,"ravindu2232",
+        jwt.verify(token,process.env.MONGO_URL,
             (err,decoded)=>{
                 if(!err){
                    req.user = decoded;
